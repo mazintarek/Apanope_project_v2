30084326b57ce310ecc5ee295f5bf9371554b4ac
mazintarek <158521579+mazintarek@users.noreply.github.com>
2026-01-18 17:15:49 +0200

    Add files via upload

---
+++ Js/waves.js
@@ -0,0 +1,17 @@
+
+function updateHeaderHeight() {
+    const header = document.querySelector('header');
+    if (!header) return;
+    const h = header.offsetHeight;
+    document.documentElement.style.setProperty('--header-height', (h + 100) + 'px');
+}
+window.addEventListener('load', updateHeaderHeight);
+window.addEventListener('resize', updateHeaderHeight);
+
+
+// let el = document.querySelector(".scroller");
+// let height = document.documentElement.scrollHeight - document.documentElement.clientHeight + 250;
+// addEventListener("scroll", () => {
+//     let scrollTop = document.documentElement.scrollTop;
+//     el.style.width = `${(scrollTop / height) * 100}%`
+// })
